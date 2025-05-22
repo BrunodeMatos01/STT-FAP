05b307592c63992f417c16930aeee98b6e2c10a5
BrunodeMatos01 <brunodematos3@gmail.com>
2025-05-22 08:32:00 -0300

    Arrumado

--- FrontEnd/STTFAP/src/app/login/login.component.ts
+++ FrontEnd/STTFAP/src/app/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
