b5f33304c953b4c24093cf3d65cae678aa0a3dba
BrunodeMatos01 <brunodematos3@gmail.com>
2025-05-22 08:57:49 -0300

    novo Back

---
+++ BackEnd/STTFAP/src/main/java/com/example/STTFAP/STTFAP/SttfapApplication.java
@@ -0,0 +1,13 @@
+package com.example.STTFAP.STTFAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttfapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SttfapApplication.class, args);
+	}
+
+}
